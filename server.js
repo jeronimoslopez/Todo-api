192d6126caea9c51c63aa0f407395501221fa7ad
Jeronimo Lopez <jeronimoslopez@gmail.com>
2016-09-24 23:43:53 -0500

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -61,6 +61,32 @@ app.delete('/todos/:id', function (req, res) {
     }
 });
 
+app.put('/todos/:id', function (req, res) {
+    var body = _.pick(req.body, 'description', 'completed');
+    var todoId = parseInt(req.params.id, 10);
+    var todoItem = _.findWhere(todos, {id: todoId});
+    var validAttributes = {};
+
+    if (!todoItem) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(todoItem, validAttributes);
+    res.json(todoItem);
+});
+
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
 });
